dbac64380692acae89349c986f5e69cc92b52754
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-13 02:48:10 +0300

    added spawner and gamefield components

---
+++ components/gamefield.go
@@ -0,0 +1,15 @@
+components {
+  id: "gamefield"
+  component: "/scripts/components/gamefield.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ components/spawner.go
@@ -0,0 +1,15 @@
+components {
+  id: "spawner"
+  component: "/scripts/components/spawner.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
