6aea3b334759a0fcc7a8434c75abbf791be25218
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-16 13:51:25 +0300

    rename gameobjects directory to prefabs

--- code/config.lua
+++ code/config.lua
@@ -1,11 +1,13 @@
-local M = {}
+local config = {}
 
 -- Objects
-M.MENU = hash("menu")
-M.GAME = hash("game")
-M.CAMERA = hash("/camera")
+config.MENU = hash("menu")
+config.GAME = hash("game")
+config.CAMERA = hash("/camera")
 
 -- Events
-M.LOAD_SCENE = hash("load_scene")
+config.LOAD_SCENE = hash("load_scene")
+config.SPAWN_PACK = hash("spawn_pack")
+config.SPAWN_UNIT = hash("spawn_unit")
 
-return M
+return config
