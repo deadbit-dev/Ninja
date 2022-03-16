9105e8acab1eff8eed7ab1f3959875d5eb35a6f5
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-17 00:01:28 +0300

    some configuration for spawning

--- code/config.lua
+++ code/config.lua
@@ -1,3 +1,5 @@
+local SpawnZone = require "modules.spawn.spawnzone"
+local SpawnPack = require "modules.spawn.spawnzone"
 local config = {}
 
 -- Objects
@@ -6,8 +8,35 @@ config.GAME = hash("game")
 config.CAMERA = hash("/camera")
 
 -- Events
-config.LOAD_SCENE = hash("load_scene")
-config.SPAWN_PACK = hash("spawn_pack")
-config.SPAWN_UNIT = hash("spawn_unit")
+config.ON_LOAD_SCENE = hash("load_scene")
+config.ON_SPAWN_START = hash("spawn_start")
+config.ON_SPAWN_END = hash("spawn_end")
+config.ON_SPAWN_PACK = hash("spawn_pack")
+config.ON_SPAWN_UNIT = hash("spawn_unit")
+
+-- SpawnZones
+config.SPAWN_ZONES = {
+  BOTTOM = SpawnZone(
+    vmath.vector3(0.0, 0.0, 50.0),
+    vmath.vector3(1.0, 0.0, 130.0)
+  ),
+  LEFT = SpawnZone(
+    vmath.vector3(0.0, 0.0, 50.0),
+    vmath.vector3(0.0, 0.25, 35.0)
+  ),
+  RIGHT = SpawnZone(
+    vmath.vector3(1.0, 0.0, 130.0),
+    vmath.vector3(1.0, 0.25, 145.0)
+  )
+}
+
+-- SpawnInfo
+config.SPAWN_INTERVAL = 2.5
+config.UNITS_BY_PACK = 5
+
+-- SpawnPack
+config.SPAWN_PACK = SpawnPack({
+
+})
 
 return config
