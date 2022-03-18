4281fe9e8e9e45497bd6e588549a5ecad8865eee
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-18 04:22:22 +0300

    update math module

--- code/modules/utils/math.lua
+++ code/modules/utils/math.lua
@@ -1,3 +1,27 @@
-local math = {}
+local Math = {}
 
-return math
+function Math.sum(array)
+  local acc = 0
+
+  for _, value in ipairs(array) do
+    acc = acc + value
+  end
+
+  return acc
+end
+
+function Math.random_range_weight(weights)
+  local point = math.random() * Math.sum(weights)
+
+  for i, value in ipairs(weights) do
+    if point < value then
+      return i
+    end
+
+    point = point - value
+  end
+
+  return #weights
+end
+
+return Math
