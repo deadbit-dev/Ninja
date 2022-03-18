19fd270928dc6c57b10a049cc25f29196cf5b2f8
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-18 13:24:51 +0300

    added prefab for spawnpack

---
+++ prefabs/spawnpack.go
@@ -0,0 +1,108 @@
+embedded_components {
+  id: "apple"
+  type: "factory"
+  data: "prototype: \"/prefabs/units/apple.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "cherry"
+  type: "factory"
+  data: "prototype: \"/prefabs/units/cherry.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "limon"
+  type: "factory"
+  data: "prototype: \"/prefabs/units/limon.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "pear"
+  type: "factory"
+  data: "prototype: \"/prefabs/units/pear.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "strawberry"
+  type: "factory"
+  data: "prototype: \"/prefabs/units/strawberry.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "watermelon"
+  type: "factory"
+  data: "prototype: \"/prefabs/units/watermelon.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
