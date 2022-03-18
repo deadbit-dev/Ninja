d855e8014294bcaa78cc9435f41f262af6ba5847
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-18 04:25:14 +0300

    Update config.lua

--- code/config.lua
+++ code/config.lua
@@ -1,42 +1,52 @@
-local SpawnZone = require "modules.spawn.spawnzone"
-local SpawnPack = require "modules.spawn.spawnzone"
+local Field = require "code.modules.utils.field"
+local Pack = require "code.modules.spawn.pack"
+local Zone = require "code.modules.spawn.zone"
+local Point = require "code.modules.spawn.point"
+local Unit = require "code.modules.spawn.unit"
 local config = {}
 
--- Objects
-config.MENU = hash("menu")
-config.GAME = hash("game")
-config.CAMERA = hash("/camera")
-
--- Events
+-- Event
 config.ON_LOAD_SCENE = hash("load_scene")
-config.ON_SPAWN_START = hash("spawn_start")
-config.ON_SPAWN_END = hash("spawn_end")
-config.ON_SPAWN_PACK = hash("spawn_pack")
+config.ON_INIT_SCENE = hash("init_scene")
+config.ON_START_SCENE = hash("start_scene")
+
+config.ON_START_SPAWN = hash("start_spawn")
+config.ON_STOP_SPAWN = hash("stop_spawn")
 config.ON_SPAWN_UNIT = hash("spawn_unit")
 
--- SpawnZones
-config.SPAWN_ZONES = {
-  BOTTOM = SpawnZone(
-    vmath.vector3(0.0, 0.0, 50.0),
-    vmath.vector3(1.0, 0.0, 130.0)
+-- Object
+config.CAMERA = hash("/camera")
+config.GAME_FIELD = Field(config.CAMERA)
+
+-- Spawn
+config.SPAWN_ZONES = Pack("", {
+  Zone(
+    Point(vmath.vector3(0.0, 0.0, 0), 50.0),
+    Point(vmath.vector3(1.0, 0.0, 0), 130.0),
+    50
   ),
-  LEFT = SpawnZone(
-    vmath.vector3(0.0, 0.0, 50.0),
-    vmath.vector3(0.0, 0.25, 35.0)
+  Zone(
+    Point(vmath.vector3(0.0, 0.0, 0), 50.0),
+    Point(vmath.vector3(0.0, 0.25, 0), 35.0),
+    25
   ),
-  RIGHT = SpawnZone(
-    vmath.vector3(1.0, 0.0, 130.0),
-    vmath.vector3(1.0, 0.25, 145.0)
+  Zone(
+    Point(vmath.vector3(1.0, 0.0, 0), 130.0),
+    Point(vmath.vector3(1.0, 0.25, 0), 145.0),
+    25
   )
-}
-
--- SpawnInfo
-config.SPAWN_INTERVAL = 2.5
-config.UNITS_BY_PACK = 5
-
--- SpawnPack
-config.SPAWN_PACK = SpawnPack({
-
 })
+
+config.SPAWN_PACK = Pack("spawnpack", {
+  Unit("apple", 50),
+  Unit("cherry", 50),
+  Unit("limon", 50),
+  Unit("pear", 50),
+  Unit("strawberry", 50),
+  Unit("watermelon", 50)
+})
+
+config.SPAWN_INTERVAL = 3
+config.UNITS_BY_PACK = 1
 
 return config
