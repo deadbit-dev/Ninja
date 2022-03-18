a19a84416864d231e09806e636f2e66fcabbe2d8
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-18 22:39:41 +0300

    remove private logic

--- code/modules/spawn/zone.lua
+++ code/modules/spawn/zone.lua
@@ -1,33 +1,22 @@
 local class = require "code.modules.utils.middleclass"
-local Point = require "code.modules.spawn.point"
+local Point = require "code.modules.cammon.point"
 local Zone = class('Zone')
 
-local _private = setmetatable({}, {__mode = "k"})
-
 function Zone:initialize(pointA, pointB, percent)
-  _private[self] = {
-    pointA = pointA,
-    pointB = pointB,
-    percent = percent
-  }
-end
-
-function Zone:get_percent() 
-  return _private[self].percent
+  self.pointA = pointA
+  self.pointB = pointB
+  self.percent = percent
 end
 
 function Zone:get_random_point()
-  local pointA = _private[self].pointA
-  local pointB = _private[self].pointB
+  local pointA = self.pointA
+  local pointB = self.pointB
 
-  local posA = pointA:get_pos()
-  local posB = pointB:get_pos()
+  local weight = math.random()
+  local pos = vmath.slerp(weight, pointA.pos, pointB.pos)
+  local angle = vmath.lerp(weight, pointA.angle, pointB.angle)
 
-  local v1 = vmath.vector3(posA.x, posA.y, pointA:get_angle())
-  local v2 = vmath.vector3(posB.x, posB.y, pointB:get_angle())
-  local v3 = vmath.slerp(math.random(), v1, v2)
-
-  return Point(vmath.vector3(v3.x, v3.y, 0), v3.z)
+  return Point(pos, angle)
 end
 
 return Zone
--- code/modules/utils/field.lua
+++ code/modules/utils/field.lua
@@ -1,18 +1,14 @@
 local class = require "code.modules.utils.middleclass"
 local camera = require "orthographic.camera"
 
-local _private = setmetatable({}, {__mode = "k"})
-
 local Field = class('Field')
 
 function Field:initialize(camera_id)
-  _private[self] = {
-    camera_id = camera_id
-  }
+  self.camera_id = camera_id
 end
 
 function Field:screen_to_field(pos)
-  local camera_id = _private[self].camera_id
+  local camera_id = self.camera_id
   local viewport = camera.get_viewport(camera_id)
   local screen = vmath.vector3(pos.x * viewport.z, pos.y * viewport.w, pos.z)
 
