c3eae53c40dd796ed62b11b5754e4b828a4bdaab
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-20 02:14:50 +0300

    Update unit.lua

--- code/modules/cammon/unit.lua
+++ code/modules/cammon/unit.lua
@@ -1,9 +1,14 @@
 local class = require "code.modules.utils.middleclass"
 local Unit = class("Unit")
 
-function Unit:initialize(config, param)
-  for key, value in pairs(config) do
-    self[key] = param[key] or value
+function Unit:initialize(id, default, modification)
+  self.id = id
+  self.modification = default
+
+  if modification then
+    for key, value in pairs(modification) do
+      self.modification[key] = value
+    end
   end
 end
 
