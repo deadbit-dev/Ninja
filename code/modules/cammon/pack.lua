abd10d6069110b371f938c501c0028948aae21c8
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-20 02:14:44 +0300

    Update pack.lua

--- code/modules/cammon/pack.lua
+++ code/modules/cammon/pack.lua
@@ -2,20 +2,20 @@ local class = require "code.modules.utils.middleclass"
 local Math = require "code.modules.utils.math"
 local Pack = class('Pack')
 
-function Pack:initialize(id, units)
+function Pack:initialize(id, values)
   self.id = id
-  self.units = units
+  self.values = values
 end
 
 function Pack:get_random_value()
-  local units = self.units
+  local values = self.values
 
   local weights = {}
-  for i = 1, #units do
-    table.insert(weights, i, units[i].percent)
+  for i = 1, #values do
+    table.insert(weights, i, values[i].weight or 0)
   end
 
-  return units[Math.random_range_weight(weights)]
+  return values[Math.random_range_weight(weights)].value
 end
 
 return Pack
