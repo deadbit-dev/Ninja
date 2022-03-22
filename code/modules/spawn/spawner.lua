1f1f68e3e8280b7d15132d2224a306510cecea6c
Pavel <pavelpetrochenko2001@gmail.com>
2022-03-22 13:08:31 +0300

    Update spawner.lua

--- code/modules/spawn/spawner.lua
+++ code/modules/spawn/spawner.lua
@@ -1,4 +1,5 @@
 local class = require "code.modules.utils.middleclass"
+local Unit = require "code.modules.cammon.unit"
 local Spawner = class('Spawner')
 
 function Spawner:initialize(gamefield, zones)
@@ -6,14 +7,17 @@ function Spawner:initialize(gamefield, zones)
   self.zones = zones
 end
 
-function Spawner:spawn(prefab)
-  local point = self.zones:get_random_value().value:get_random_point()
+function Spawner:spawn(data)
+  local zone = self.zones:get_random_value()
+  local point = zone.value:get_random_point()
   local pos = self.gamefield:screen_to_field(point.pos)
+  local view = factory.create(data.url)
+  local angle = point.angle
 
-  local unit = factory.create(prefab, pos)
-  go.set(unit, "euler.z", point.angle)
+  data.meta.position = pos
+  data.meta.velocity = vmath.vector3(math.cos(angle), math.sin(angle), 0) * data.meta.force
 
-  return unit 
+  return Unit(view, data.meta)
 end
 
 return Spawner
