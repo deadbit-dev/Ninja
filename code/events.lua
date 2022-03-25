8b9a48b3bc3c4fc2e14b9e0047018239a266935a
Pavel <pavelpetrochenko2001@gmail.com>
2022-03-25 10:06:13 +0300

    added some events

---
+++ code/events.lua
@@ -0,0 +1,9 @@
+local Events = {}
+
+Events.ON_LOAD = hash("load")
+Events.ON_TOUCH_START = hash("touch_start")
+Events.ON_TOUCH_PROCESS = hash("touch_process")
+Events.ON_TOUCH_END = hash("touch_end")
+Events.ON_GENERATED = hash("generated")
+
+return Events
