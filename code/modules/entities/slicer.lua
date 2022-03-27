f008aa325632f88e0040452e5207e16dfde51da3
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-27 11:28:16 +0300

    added function set_position for slicer view

--- code/modules/entities/slicer.lua
+++ code/modules/entities/slicer.lua
@@ -2,12 +2,17 @@ local class = require "code.modules.utils.middleclass"
 local Slicer = class('Slicer')
 
 function Slicer:initialize(args)
-  self.id = args.id
+  self.view = args.view
   self.position = args.position or vmath.vector3()
   self.radius = args.radius or 0
   self.min_speed_for_slice = args.min_speed_for_slice or 0
   self.active = args.active or false
   self.slicing = args.slicing or false
+end
+
+function Slicer:set_position(position)
+  self.position = position
+  go.set_position(position, self.view)
 end
 
 return Slicer
