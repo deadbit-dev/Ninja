5436d2629eb512d9f2b7cbbc5a97c6a42b7f6c37
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-28 02:18:16 +0300

    move constansnt to config

--- code/modules/systems/gravity_system.lua
+++ code/modules/systems/gravity_system.lua
@@ -1,16 +1,15 @@
 local class = require "code.modules.utils.middleclass"
 local GravitySystem = class("GravitySystem")
 
-local GRAVITY = 9.8
-local AXIS_GRAVITY = vmath.vector3(0, -1, 0)
-
-function GravitySystem:initialize(state)
+function GravitySystem:initialize(state, config)
   self.units = state.context.units
+  self.gravity = config.GRAVITY
+  self.gravity_axis = config.GRAVITY_AXIS
 end
 
 function GravitySystem:update(dt)
   for _, unit in ipairs(self.units) do
-    unit.velocity = unit.velocity + unit.mass * GRAVITY * AXIS_GRAVITY * dt
+    unit.velocity = unit.velocity + unit.mass * self.gravity * self.gravity_axis * dt
     unit:set_position(unit.position + unit.velocity * dt)
   end
 end
