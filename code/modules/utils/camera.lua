679d08318abc3bcfb8fc1eb64c5bf447a8455868
deadbit-dev <pavelpetrochenko2001@gmail.com>
2022-03-29 23:40:59 +0300

    fix spawn by resize screen, fix convert from window coordinates

--- code/modules/utils/camera.lua
+++ code/modules/utils/camera.lua
@@ -2,9 +2,8 @@ local Camera = {}
 
 function Camera:viewport_to_world(pos, camera_id, camera)
   local viewport = camera.get_viewport(camera_id)
-  local screen = vmath.vector3(pos.x * viewport.z, pos.y * viewport.w, pos.z)
-
-  return camera.screen_to_world(camera_id, screen)
+  local window = vmath.vector3(pos.x * viewport.z, pos.y * viewport.w, pos.z)
+  return camera.window_to_world(camera_id, window)
 end
 
 return Camera
